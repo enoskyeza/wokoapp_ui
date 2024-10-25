6f9bac053e96540d2edd5772efa032eae2f39936
enoskyeza <enoskyeza@gmail.com>
2024-10-26 01:02:44 +0300

    test pwa next docs

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,21 @@
 import type { NextConfig } from 'next';
-import nextPwa from 'next-pwa';
+// import nextPwa from 'next-pwa';
+//
+// const withPWA = nextPwa({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+// });
+//
+// // This is the fix:
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   // additional Next.js config here
+// }) as NextConfig;
 
-const withPWA = nextPwa({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-// This is the fix:
-const nextConfig = withPWA({
-  reactStrictMode: true,
+const nextConfig = {
   // additional Next.js config here
-}) as NextConfig;
+} as NextConfig;
 
 export default nextConfig;
