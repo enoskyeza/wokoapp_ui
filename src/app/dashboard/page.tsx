e20b3c9be6ece1f6c5575d900df1fcb2a0435d77
enoskyeza <enoskyeza@gmail.com>
2024-10-26 04:46:40 +0300

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,28 +5,24 @@ import Contestants from "@/components/Tables/Contestants";
 import dynamic from "next/dynamic";
 
 const AgeCategoryChart = dynamic(() => import("@/components/Dashboard/CategoryPieChart"), {
-  ssr: false,
+    ssr: false,
 });
 
 const DashboardPage = () => {
+
     return (
-
         <DashboardLayout>
-            {/*<h1 className="text-3xl font-bold text-gray-900 mb-6">Dashboard</h1>*/}
-
             {/* Statistics Cards */}
             <DashboardStats/>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
-                <AgeCategoryChart />
-                {/*<GenderChart/>*/}
-                {/*<PaymentStatusChart/>*/}
+                <AgeCategoryChart/>
             </div>
 
             {/* Participants Table */}
             <Contestants/>
         </DashboardLayout>
-    );
+    )
 };
 
 export default DashboardPage;
