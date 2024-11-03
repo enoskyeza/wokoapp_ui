cf432918ff5ba7aab4000870b21f35444d7a77f8
enoskyeza <enoskyeza@gmail.com>
2024-11-03 20:36:30 +0300

    add parent type

--- src/types.tsx
+++ src/types.tsx
@@ -17,5 +17,17 @@ export interface Participant {
     payment_status: "paid" | "not_paid";
     payment_method: PaymentMethod;
     parent: number;
+    parent_name: number;
     age_category: "junior" |"intermediate"| "senior";
+}
+
+export interface Parent {
+    id: number;
+    'first_name': string;
+    'last_name': string;
+    'profession': string;
+    'address': string;
+    'email': string;
+    'phone_number': string;
+    contestants: Participant[]
 }
