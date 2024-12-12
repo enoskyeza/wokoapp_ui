d2d7c1c0cad2be474d9dd7f8470f433a363adc68
enoskyeza <enoskyeza@gmail.com>
2024-12-12 17:27:11 +0300

    fix build error

--- src/components/Dashboard/Judge.tsx
+++ src/components/Dashboard/Judge.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import {useParticipantContext} from "@/context/ParticipantContext";
-import { Participant } from "@/types";
 import Cookies from "js-cookie";
 
 
