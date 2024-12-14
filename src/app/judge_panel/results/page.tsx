09b0b154bd7653ed4eb827c69dcd64579463dda8
enoskyeza <enoskyeza@gmail.com>
2024-12-14 13:46:56 +0300

    activate results section

--- src/app/judge_panel/results/page.tsx
+++ src/app/judge_panel/results/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState, useEffect } from "react";
-import { PaginatedResults, Contestant } from "@/types";
+import { PaginatedResults } from "@/types";
 import { API_URL } from "@/config";
 import ResultsTable from "@/components/Tables/Results";
 
