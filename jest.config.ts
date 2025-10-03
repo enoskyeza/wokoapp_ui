ef622344038fb9830dd25e47b6ed7af5fc73460b
enoskyeza <enoskyeza@gmail.com>
2025-10-03 05:46:52 +0300

    update libraries, add jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/**/types.ts'],
+}
+
+export default createJestConfig(customJestConfig)
